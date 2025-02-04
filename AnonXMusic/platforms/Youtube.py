a6621565e56cd26927c8eb9e6d3f5c4afb271437
Anjlnobita <jiteshkumar7200@gmail.com>
2025-02-04 17:15:19 +0530

    Update Youtube.py

--- AnonXMusic/platforms/Youtube.py
+++ AnonXMusic/platforms/Youtube.py
@@ -8,7 +8,7 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from AnomXMusic.utils.database import is_on_off
+from AnonXMusic.utils.database import is_on_off
 from AnonXMusic.utils.formatters import time_to_seconds
 
 async def shell_cmd(cmd):
